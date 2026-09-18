064bf1512bbd38cdbf2af393e9693764fd0c3c0e
Juan Diego <juandiegopossohidalgo@gmail.com>
2026-09-18 06:23:45 -0500

    feat se crea proyecto

---
+++ beta_1_proyecto/src/main/java/com/example/beta_1_proyecto/Beta1ProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.example.beta_1_proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beta1ProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beta1ProyectoApplication.class, args);
+	}
+
+}
